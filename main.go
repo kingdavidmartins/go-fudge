6bac8b8f05bc43b5910037d923347b929cbfd06d
King <kingdavidmartins@users.noreply.github.com>
2018-02-28 00:06:03 -0500

    finished go | brainfuck-inter

--- main.go
+++ main.go
@@ -24,6 +24,68 @@ func main() {
 		panic(err)
 	}
 
-	// prints hello.bf as bytes
-	fmt.Println(bytes)
+	// use recursion to scan through each token
+	InterpreteProgram(bytes)
+}
+
+// create empty int variable array
+var memory = make([]int, 512)
+var ptr = 0
+
+// InterpreteProgram runs said cases recursively if memory[ptr] != 0
+func InterpreteProgram(bytes []byte) {
+	loop := []byte{}
+	looping := false
+	jump2end := false
+
+	for _, ch := range bytes {
+		s := string(ch)
+
+		if s != "]" && jump2end {
+			continue
+		}
+
+		if looping {
+			loop = append(loop, ch)
+		}
+
+		switch s {
+		case ">":
+			ptr++
+		case "<":
+			if ptr != 0 {
+				ptr--
+			}
+		case ".":
+			fmt.Printf("%s", string(memory[ptr]))
+		case "+":
+			if memory[ptr] == 255 {
+				memory[ptr]--
+			}
+			memory[ptr]++
+		case "-":
+			if memory[ptr] == 0 {
+				memory[ptr] = 256
+			}
+			memory[ptr]--
+		case "[":
+			if memory[ptr] == 0 {
+				jump2end = true
+				continue
+			}
+
+			looping = true
+			loop = append([]byte{}, ch)
+		case "]":
+			if jump2end {
+				jump2end = false
+			}
+
+			if memory[ptr] != 0 {
+				InterpreteProgram(loop)
+			}
+
+			looping = false
+		}
+	}
 }
