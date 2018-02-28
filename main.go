184fffdfbb0a5fa68bc06f3bad77664acfc3e767
King <kingdavidmartins@users.noreply.github.com>
2018-02-27 22:52:44 -0500

    add hello.bf and readout bytes of file

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io/ioutil"
+	"os"
 )
 
 func main() {
-	fmt.Print("Hello World")
+	// get filename from cmd args
+	fn := os.Args[1]
+
+	// open file
+	fd, err := os.Open(fn)
+	if err != nil {
+		panic(err)
+	}
+
+	// put file into string
+	reader := bufio.NewReader(fd)
+	bytes, err := ioutil.ReadAll(reader)
+	if err != nil {
+		panic(err)
+	}
+
+	// prints hello.bf as bytes
+	fmt.Println(bytes)
 }
